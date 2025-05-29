327856c7bd3a259e255fcab97a989a0a89b4279a
Matheus Picolli Ishibashi <matheus.ishibashi@gmail.com>
2025-05-29 20:17:57 -0300

    Models Criados
    Criação da classe Produto e algumas outras

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -4,10 +4,89 @@
  */
 package model;
 
-/**
- *
- * @author Dell
- */
 public class Produto {
-    
+
+    private int id;
+    private String nome;
+    private double preco;
+    private UnidadeMedida unidadeMedida;
+    private Categoria categoria;
+    private int quantidadeEstoque;
+    private int quantidadeMinima;
+    private int quantidadeMaxima;
+
+    public Produto(String nome, double preco, UnidadeMedida unidadeMedida, Categoria categoria, int quantidadeEstoque, int quantidadeMinima, int quantidadeMaxima) {
+        this.nome = nome;
+        this.preco = preco;
+        this.unidadeMedida = unidadeMedida;
+        this.categoria = categoria;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.quantidadeMinima = quantidadeMinima;
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public UnidadeMedida getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public void setUnidadeMedida(UnidadeMedida unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public int getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public void setQuantidadeMaxima(int quantidadeMaxima) {
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
 }
---
+++ src/main/java/model/TamanhoProduto.java
@@ -0,0 +1,23 @@
+package model;
+
+public enum TamanhoProduto {
+    PEQUENO("Pequeno"),
+    MEDIO("Médio"),
+    GRANDE("Grande");
+
+    private String descricao;
+
+    private TamanhoProduto(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return this.descricao;
+    }
+    
+}
