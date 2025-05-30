0b83c4fe614d95e1fe6093abda549acd65f793ed
Matheus Picolli Ishibashi <matheus.ishibashi@gmail.com>
2025-05-30 16:43:11 -0300

    Commit de Teste
    Apenas Testando

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -4,10 +4,22 @@
  */
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Produto;
+
 /**
  *
  * @author Dell
  */
 public class ProdutoDAO {
+
+    List<Produto> produtos = new ArrayList<>();
+
+    public boolean adicionarProduto(Produto produto) {
+        produtos.add(produto);
+        return true;
+    }
     
 }
