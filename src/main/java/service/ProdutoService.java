a748815cb6b5c50223f2bff45edc39ca8dba65df
Luanlmeida <luanlmeidaa7@gmail.com>
2025-06-04 20:03:04 -0300

    Atualização ProdutoService
    ProdutoService incrementado cadastrarProduto e atualizarProduto

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -1,9 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package service;
 
+import java.util.List;
+import dao.ProdutoDAO;
+import model.Produto;
+
 public class ProdutoService {
-    
+    private ProdutoDAO produtoDAO;
+
+    public ProdutoService() {
+        produtoDAO = new ProdutoDAO();
+    }
+
+}
+
+    public boolean cadastrarProduto(Produto produto) {
+        if (produto != null && produto.getNome() != null && produto.getNome().isEmpty()) {
+            // return produtoDAO.inserir(produto);                              PRECISA COLOCAR INSERIR ATUALIZAR NO PRODUTODAO
+        }
+    }
+
+public boolean atualizarProduto(Produto produto) {
+    if (produto != null && produto.getId() > 0) {
+//        return produtoDAO.atualizar(produto);                                   PRECISA COLOCAR INSERIR ATUALIZAR NO PRODUTODAO
+    }
+    return false;
 }
