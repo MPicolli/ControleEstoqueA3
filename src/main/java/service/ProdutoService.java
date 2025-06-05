33ce769e836a39a928aea64cc33c22c01fd1b643
Luanlmeida <luanlmeidaa7@gmail.com>
2025-06-04 21:18:28 -0300

    Update ProdutoService.java

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -13,29 +13,29 @@ public class ProdutoService {
 
     public boolean cadastrarProduto(Produto produto) {
         if (produto != null && produto.getNome() != null && produto.getNome().isEmpty()) {
-//        return produtoDAO.inserir(produto);                                     PRECISA COLOCAR INSERIR  NO PRODUTODAO
+        return produtoDAO.inserir(produto);                                     //PRECISA COLOCAR INSERIR  NO PRODUTODAO
         }
     }
 
     public boolean atualizarProduto(Produto produto) {
     if (produto != null && produto.getId() > 0) {
-//        return produtoDAO.atualizar(produto);                                   PRECISA COLOCAR ATUALIZAR NO PRODUTODAO
+        return produtoDAO.atualizar(produto);                                   //PRECISA COLOCAR ATUALIZAR NO PRODUTODAO
     }
     return false;
 }
 
     public boolean excluirProduto(int id){
         if (id > 0) {
-//            return produtoDAO.excluir(id);                                   PRECISA COLOCAR EXCLUIR NO PRODUTODAO
+            return produtoDAO.excluir(id);                                   //PRECISA COLOCAR EXCLUIR NO PRODUTODAO
         }
             return false;
     }
     public List<Produto> listarProdutos(){
-//        return produtoDAO.buscarTodosProdutos;                                 PRECISA COLOCAR BUSCARTODOSPRODUTOS NO PRODUTODAO
+        return produtoDAO.buscarTodosProdutos();                                 //PRECISA COLOCAR BUSCARTODOSPRODUTOS NO PRODUTODAO
     }
 
     public Produto buscarPorId(int id) {
-//        return produtoDAO.buscarPorId(id);                                 PRECISA COLOCAR BUSCARPORID NO PRODUTODAO
+        return produtoDAO.buscarPorId(id);                                 //PRECISA COLOCAR BUSCARPORID NO PRODUTODAO
     }
 
         public void reajustarPreco(double percentual) {
