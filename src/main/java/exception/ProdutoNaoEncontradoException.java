9612b49a82950f5258191b750a8dad44b6e4df6b
arthurzand <arthurzand@gmail.com>
2025-06-05 20:22:07 -0300

    Update ProdutoNaoEncontradoException.java

--- src/main/java/exception/ProdutoNaoEncontradoException.java
+++ src/main/java/exception/ProdutoNaoEncontradoException.java
@@ -1,5 +1,12 @@
 package exception;
 
-public class ProdutoNaoEncontradoException {
-    
+public class ProdutoNaoEncontradoException extends RuntimeException {
+
+    public ProdutoNaoEncontradoException(String message) {
+        super(message);
+    }
+
+    public ProdutoNaoEncontradoException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
