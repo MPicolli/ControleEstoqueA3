ef5816861745f5f425a40e6825f25d134ea6bd4b
Jonathan Schiestl <jonathan.wesley.rs@gmail.com>
2025-06-05 09:30:39 +0200

    Atualizaçao de tabelas
    Adicionei tabela movimentacao de estoque

--- sql/controle_estoque.sql
+++ sql/controle_estoque.sql
@@ -3,15 +3,15 @@ CREATE DATABASE controle_estoque;
 USE controle_estoque;
 
 CREATE TABLE tb_categoria (
-    id INT NOT NULL AUTO_INCREMENT,
+    id_categoria INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     tamanho_produto ENUM('Pequeno', 'Médio', 'Grande'),
     tipo_embalagem ENUM('Lata', 'Vidro', 'Plástico'),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id_categoria)
 ) ENGINE = InnoDB;
 
 CREATE TABLE tb_produto (
-    id INT NOT NULL AUTO_INCREMENT,
+    id_produto INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     preco DECIMAL(10, 2) NOT NULL,
     unidade_medida ENUM('Unidade', 'Quilograma', 'Litro', 'Caixa') NOT NULL,
@@ -19,7 +19,17 @@ CREATE TABLE tb_produto (
     quantidade_estoque INT NOT NULL,
     quantidade_minima INT NOT NULL,
     quantidade_maxima INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_id_categoria FOREIGN KEY (id_categoria) REFERENCES tb_categoria(id)
+    PRIMARY KEY (id_produto),
+    CONSTRAINT fk_id_categoria FOREIGN KEY (id_categoria) REFERENCES tb_categoria(id_categoria)
 ) ENGINE = InnoDB;
 
+CREATE TABLE tb_movimentacao_estoque (
+    id_movimentacao INT NOT NULL AUTO_INCREMENT,
+    id_produto INT NOT NULL,
+    tipo_movimentacao VARCHAR(50) NOT NULL, -- Ex: Entrada de Compra, Saida de Venda;
+    data_movimentacao DATE NOT NULL,
+    observacoes VARCHAR(250),
+    PRIMARY KEY (id_movimentacao),
+    CONSTRAINT fk_id_produto FOREIGN KEY (id_produto) REFERENCES tb_produto(id_produto)
+    ENGINE = InnoDB;
+)
