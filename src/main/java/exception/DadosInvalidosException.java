ca41e3aeb7db5cc641a1e317d6d3306b482a0d87
arthurzand <arthurzand@gmail.com>
2025-06-05 20:21:43 -0300

    Update DadosInvalidosException.java

--- src/main/java/exception/DadosInvalidosException.java
+++ src/main/java/exception/DadosInvalidosException.java
@@ -1,30 +1,12 @@
 package exception;
 
-public class DadosInvalidosException {
-    public static void main(String[] args) {
-        try {
-            throw new DadosInvalidosException("Dados inválidos fornecidos.");
-        } catch (DadosInvalidosException e) {
-            System.out.println(e.getMensagem());
-        }
-        
-    } String mensagem;
+public class DadosInvalidosException extends RuntimeException {
 
-    public DadosInvalidosException(String mensagem) {
-        this.mensagem = mensagem;
+    public DadosInvalidosException(String message) {
+        super(message);
     }
 
-    public String getMensagem() {
-        if (mensagem == null || mensagem.isEmpty()) {
-            return "Nenhuma mensagem de erro fornecida.";
-        }
-        return mensagem;
-    }
-
-    public void setMensagem(String mensagem) {
-        if (mensagem == null || mensagem.isEmpty()) {
-            throw new IllegalArgumentException("Mensagem não pode ser nula ou vazia.");
-        }
-        this.mensagem = mensagem;
+    public DadosInvalidosException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
