7f1ae36a192e08f3585721ba6d23ec8add8fbda5
Luanlmeida <luanlmeidaa7@gmail.com>
2025-06-04 21:15:08 -0300

    Atualização ProdutoService

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -11,8 +11,6 @@ public class ProdutoService {
         produtoDAO = new ProdutoDAO();
     }
 
-}
-
     public boolean cadastrarProduto(Produto produto) {
         if (produto != null && produto.getNome() != null && produto.getNome().isEmpty()) {
 //        return produtoDAO.inserir(produto);                                     PRECISA COLOCAR INSERIR  NO PRODUTODAO
@@ -39,3 +37,34 @@ public class ProdutoService {
     public Produto buscarPorId(int id) {
 //        return produtoDAO.buscarPorId(id);                                 PRECISA COLOCAR BUSCARPORID NO PRODUTODAO
     }
+
+        public void reajustarPreco(double percentual) {
+        List<Produto> produtos = listarProdutos();
+        for (Produto produto : produtos) {
+            double novoPreco = produto.getPreco() * (1 + percentual / 100);
+            produto.setPreco(novoPreco);
+            atualizarProduto(produto);
+        }
+    }
+
+    public boolean estaAbaixoDoMinimo(Produto produto) {
+        return produto.getQuantidadeEstoque() < produto.getQuantidadeMinima();
+    }
+
+    public boolean estaAcimaDoMaximo(Produto produto) {
+        return produto.getQuantidadeEstoque() > produto.getQuantidadeMaxima();
+    }
+
+    public boolean movimentarEstoque(int idProduto, int quantidade, boolean entrada) {
+        Produto produto = buscarPorId(idProduto);
+        if (produto == null)
+            return false;
+
+        int novaQuantidade = entrada
+                ? produto.getQuantidadeEstoque() + quantidade
+                : produto.getQuantidadeEstoque() - quantidade;
+
+        produto.setQuantidadeEstoque(novaQuantidade);
+        return atualizarProduto(produto);
+    }
+}
