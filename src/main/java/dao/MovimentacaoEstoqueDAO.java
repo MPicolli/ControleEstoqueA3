6176bc04671961cdd033637fe581dd754fc6ffca
biel <gabrielmachadolv@icloud.com>
2025-06-06 13:01:47 -0300

    SuppressWarnings FieldMayBeFinal

--- src/main/java/dao/MovimentacaoEstoqueDAO.java
+++ src/main/java/dao/MovimentacaoEstoqueDAO.java
@@ -15,7 +15,8 @@ import model.TipoMovimentacao;
 
 public class MovimentacaoEstoqueDAO {
 
-    private final Connection conexao;
+    @SuppressWarnings("FieldMayBeFinal")
+    private Connection conexao;
 
     public MovimentacaoEstoqueDAO() {
         this.conexao = ConnectionFactory.getConnection();
